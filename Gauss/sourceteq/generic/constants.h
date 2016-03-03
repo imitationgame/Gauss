7ec1ba8df7232a5e23a04581e8c3c9b87b247b4a
vauxhall <zero.griffin@gmail.com>
2016-03-03 14:20:38 -0600

    Colors

--- Gauss/sourceteq/generic/constants.h
+++ Gauss/sourceteq/generic/constants.h
@@ -1,7 +1,7 @@
 // color
 
-#define colormain               [UIColor colorWithRed:0.2666 green:0.3333 blue:0.4 alpha:1]
-#define colorsecond             [UIColor colorWithRed:0.5607 green:0.6 blue:0.6392 alpha:1]
+#define colormain               [UIColor colorWithRed:0.84313 green:0.21568 blue:0.26274 alpha:1]
+#define colorsecond             [UIColor colorWithRed:0.8549 green:0.8549 blue:0.8549 alpha:1]
 
 // font
 
