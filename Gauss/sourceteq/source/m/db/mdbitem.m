fa42772db804a7298098f4679550ad7658361364
vauxhall <zero.griffin@gmail.com>
2016-03-03 12:51:35 -0600

    project setup

--- Gauss/sourceteq/source/m/db/mdbitem.m
+++
@@ -1,5 +0,0 @@
-#import "mdbitem.h"
-
-@implementation mdbitem
-
-@end
--- Gauss/sourceteq/source/s/s.h
+++
@@ -1,5 +0,0 @@
-#import "sconfiglocprotocol.h"
-#import "sconfigloccountry.h"
-#import "sconfiglocstate.h"
-#import "sconfigloccity.h"
-#import "sconfiglocneigh.h"
