6966c75c5fcf73707dcccbbe22dd928c437e15b3
vauxhall <zero.griffin@gmail.com>
2016-03-03 13:05:43 -0600

    Project setup

--- Gauss/sourceteq/source/c/main/cpages.m
+++ Gauss/sourceteq/source/c/main/cpages.m
@@ -33,7 +33,6 @@
 }
 
 #pragma mark actions
-
 #pragma mark functionality
 
 -(void)changecontroller:(UIViewController*)controller direction:(UIPageViewControllerNavigationDirection)direction animated:(BOOL)animated
