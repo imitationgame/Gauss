724374185aaa6f211c355d6e9e4f73a13d91dae2
vauxhall <zero.griffin@gmail.com>
2016-03-03 21:49:11 -0600

    db create

--- Gauss/sourceteq/source/m/db/mdbcreate.m
+++ Gauss/sourceteq/source/m/db/mdbcreate.m
@@ -8,12 +8,44 @@
     db *dbcon = [db begin];
     
     query = @"CREATE TABLE course (id INTEGER PRIMARY KEY, available INTEGER, uniqueid TEXT COLLATE NOCASE);";
-    
     [dbcon query:query];
     
-    query = @"CREATE TABLE chapter (id INTEGER PRIMARY KEY, );";
+    query = @"CREATE TABLE chapter (id INTEGER PRIMARY KEY, uniqueid TEXT COLLATE NOCASE, timestamp INTEGER, score INTEGER);";
+    [dbcon query:query];
+    
+    [mdbcreate addcourse:dbcon class:[mcourseitemadd class] available:YES];
+    [mdbcreate addcourse:dbcon class:[mcourseitemsubs class] available:YES];
+    
+    [mdbcreate addchapter:dbcon class:[mcourseadd1 class]];
+    [mdbcreate addchapter:dbcon class:[mcourseadd2 class]];
+    [mdbcreate addchapter:dbcon class:[mcoursesubs1 class]];
+    [mdbcreate addchapter:dbcon class:[mcoursesubs2 class]];
     
     [dbcon commit];
+}
+
++(void)addcourse:(db*)dbcon class:(Class)class available:(BOOL)available
+{
+    NSString *uniqueid = NSStringFromClass(class);
+    NSString *query = [NSString stringWithFormat:
+                       @"INSERT INTO course "
+                       "(available, uniqueid) "
+                       "values(%@, \"%@\");",
+                       @(available), uniqueid];
+    
+    [dbcon query:query];
+}
+
++(void)addchapter:(db*)dbcon class:(Class)class
+{
+    NSString *uniqueid = NSStringFromClass(class);
+    NSString *query = [NSString stringWithFormat:
+                       @"INSERT INTO chapter "
+                       "(uniqueid, timestamp, score) "
+                       "values(\"%@\", 0, 0);",
+                       uniqueid];
+    
+    [dbcon query:query];
 }
 
 @end
