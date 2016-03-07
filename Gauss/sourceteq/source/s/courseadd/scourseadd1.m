56c14be7633131548943fd8b1486a445584a3b34
vauxhall <zero.griffin@gmail.com>
2016-03-07 11:09:14 -0600

    strategy course add 1

---
+++ Gauss/sourceteq/source/s/courseadd/scourseadd1.m
@@ -0,0 +1,12 @@
+#import "scourseadd1.h"
+
+@implementation scourseadd1
+
+-(instancetype)init
+{
+    self = [super init];
+    
+    return self;
+}
+
+@end
---
+++ Gauss/sourceteq/source/s/s.h
@@ -0,0 +1,1 @@
+#import "scourseadd1.h"
