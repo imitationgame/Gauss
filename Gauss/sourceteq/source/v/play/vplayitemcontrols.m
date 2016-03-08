67fe138cb7d598ce5871e82de69ffdece99b9c6b
vauxhall <zero.griffin@gmail.com>
2016-03-08 10:57:34 -0600

    button next

--- Gauss/sourceteq/source/v/play/vplayitemcontrols.m
+++ Gauss/sourceteq/source/v/play/vplayitemcontrols.m
@@ -24,18 +24,38 @@
     [basefield.layer setBorderWidth:1];
     [basefield.layer setBorderColor:[colorsecond colorWithAlphaComponent:0.5].CGColor];
     
+    UIButton *buttonnext = [[UIButton alloc] init];
+    [buttonnext setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [buttonnext setClipsToBounds:YES];
+    [buttonnext setImage:[[UIImage imageNamed:@"next"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate] forState:UIControlStateNormal];
+    [buttonnext setImage:[[UIImage imageNamed:@"next"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal] forState:UIControlStateHighlighted];
+    [buttonnext.imageView setContentMode:UIViewContentModeScaleAspectFit];
+    [buttonnext.imageView setClipsToBounds:YES];
+    [buttonnext.imageView setTintColor:colormain];
+    [buttonnext addTarget:self action:@selector(actionnext:) forControlEvents:UIControlEventTouchUpInside];
+    self.buttonnext = buttonnext;
+    
     [self addSubview:border];
     [self addSubview:basefield];
+    [self addSubview:buttonnext];
     
-    NSDictionary *views = @{@"border":border, @"basefield":basefield};
+    NSDictionary *views = @{@"border":border, @"basefield":basefield, @"buttonnext":buttonnext};
     NSDictionary *metrics = @{};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[border]-0-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-10-[basefield]-80-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-10-[basefield]-0-[buttonnext(80)]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[border(1)]" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-10-[basefield]-10-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[buttonnext]-0-|" options:0 metrics:metrics views:views]];
     
     return self;
+}
+
+#pragma mark actions
+
+-(void)actionnext:(UIButton*)button
+{
+    
 }
 
 @end
