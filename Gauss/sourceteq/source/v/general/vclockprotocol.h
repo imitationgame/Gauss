7116027d1893f59b6fb3ffa7e807b596ef8e7e6d
vauxhall <zero.griffin@gmail.com>
2016-03-09 08:13:18 -0600

    view clock protocol

---
+++ Gauss/sourceteq/source/v/general/vclockprotocol.h
@@ -0,0 +1,13 @@
+//
+//  vclockprotocol.h
+//  Gauss
+//
+//  Created by zero on 3/9/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol vclockprotocol <NSObject>
+
+@end
