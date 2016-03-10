08b74bfdb131ffdc008d24fcf828634fd1e43b07
vauxhall <zero.griffin@gmail.com>
2016-03-10 11:58:12 -0600

    point cell max course

---
+++ Gauss/sourceteq/source/m/playfinish/mplayfinishcellpointscellmaxcourse.m
@@ -0,0 +1,13 @@
+//
+//  mplayfinishcellpointscellmaxcourse.m
+//  Gauss
+//
+//  Created by zero on 3/10/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "mplayfinishcellpointscellmaxcourse.h"
+
+@implementation mplayfinishcellpointscellmaxcourse
+
+@end
