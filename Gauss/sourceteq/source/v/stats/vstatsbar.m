4af4bd26b668a4968717cf75e8403bf36dfc1eba
vauxhall <zero.griffin@gmail.com>
2016-03-11 15:54:23 -0600

    burger asset

--- Gauss/sourceteq/source/v/stats/vstatsbar.m
+++ Gauss/sourceteq/source/v/stats/vstatsbar.m
@@ -17,15 +17,35 @@
     [image setTintColor:[UIColor whiteColor]];
     [image setTranslatesAutoresizingMaskIntoConstraints:NO];
     
-    [self addSubview:image];
+    UIButton *buttonoptions = [[UIButton alloc] init];
+    [buttonoptions setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [buttonoptions setImage:[[UIImage imageNamed:@"burger"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal] forState:UIControlStateNormal];
+    [buttonoptions setImage:[[UIImage imageNamed:@"burger"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate] forState:UIControlStateHighlighted];
+    [buttonoptions.imageView setTintColor:[UIColor colorWithWhite:1 alpha:0.2]];
+    [buttonoptions.imageView setClipsToBounds:YES];
+    [buttonoptions.imageView setContentMode:UIViewContentModeScaleAspectFit];
+    [buttonoptions setBackgroundColor:[UIColor clearColor]];
+    [buttonoptions addTarget:self action:@selector(actionoptions:) forControlEvents:UIControlEventTouchUpInside];
     
-    NSDictionary *views = @{@"image":image};
+    [self addSubview:image];
+    [self addSubview:buttonoptions];
+    
+    NSDictionary *views = @{@"image":image, @"buttonoptions":buttonoptions};
     NSDictionary *metrics = @{};
     
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[image]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-80-[image]-80-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-15-[image(50)]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[buttonoptions(80)]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-15-[buttonoptions(50)]" options:0 metrics:metrics views:views]];
     
     return self;
+}
+
+#pragma mark actions
+
+-(void)actionoptions:(UIButton*)button
+{
+    
 }
 
 @end
