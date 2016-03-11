860b651f574cdf2d08c4e358b834640266e809bd
vauxhall <zero.griffin@gmail.com>
2016-03-11 10:46:32 -0600

    store cell

--- Gauss/sourceteq/source/v/store/vstorecell.m
+++ Gauss/sourceteq/source/v/store/vstorecell.m
@@ -1,12 +1,19 @@
 #import "vstorecell.h"
 
+static NSUInteger const labelmarginleft = 20;
+static NSUInteger const labelmarginright = 150;
+
 @implementation vstorecell
+{
+    CGFloat marginleftright;
+}
 
 -(instancetype)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     [self setClipsToBounds:YES];
     [self setBackgroundColor:[UIColor whiteColor]];
+    marginleftright = labelmarginleft + labelmarginright;
  
     UILabel *label = [[UILabel alloc] init];
     [label setBackgroundColor:[UIColor clearColor]];
@@ -18,10 +25,10 @@
     [self addSubview:label];
     
     NSDictionary *views = @{@"label":label};
-    NSDictionary *metrics = @{};
+    NSDictionary *metrics = @{@"labelmarginleft":@(labelmarginleft), @"labelmarginright":@(labelmarginright)};
     
     self.layoutlabelheight = [NSLayoutConstraint constraintWithItem:label attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:1];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[label]-150-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(labelmarginleft)-[label]-(labelmarginright)-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-20-[label]" options:0 metrics:metrics views:views]];
     [self addConstraint:self.layoutlabelheight];
     
@@ -30,7 +37,13 @@
 
 -(void)config:(mstorepurchasesitem*)item
 {
+    CGFloat width = self.bounds.size.width;
+    CGFloat labelwidth = width - marginleftright;
+    CGFloat labelheight = ([item.attributestring boundingRectWithSize:CGSizeMake(labelwidth, 1000) options:NSStringDrawingUsesFontLeading | NSStringDrawingUsesLineFragmentOrigin context:nil].size.height);
+    
     self.item = item;
+    self.layoutlabelheight.constant = labelheight;
+    self.label.attributedText = item.attributestring;
 }
 
 @end
