1d798c33d4ee00a902d56c9a53885d1b741b6c6b
vauxhall <zero.griffin@gmail.com>
2016-03-12 15:04:56 -0600

    view contact

--- Gauss/sourceteq/source/v/contact/vcontact.m
+++ Gauss/sourceteq/source/v/contact/vcontact.m
@@ -1,5 +1,22 @@
 #import "vcontact.h"
 
+@interface vcontact ()
+
+@property(weak, nonatomic)ccontact *controller;
+
+@end
+
 @implementation vcontact
+
+@dynamic controller;
+
+-(instancetype)init:(ccontact*)controller
+{
+    self = [super init:controller];
+    [self setClipsToBounds:YES];
+    [self setBackgroundColor:[UIColor whiteColor]];
+    
+    return self;
+}
 
 @end
