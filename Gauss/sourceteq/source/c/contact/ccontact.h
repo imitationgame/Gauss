76cc6d36190ef92f4be2c3b6d05fd860b6010611
vauxhall <zero.griffin@gmail.com>
2016-03-12 14:59:38 -0600

    contact controller

--- Gauss/sourceteq/source/c/contact/ccontact.h
+++ Gauss/sourceteq/source/c/contact/ccontact.h
@@ -1,13 +1,5 @@
-//
-//  ccontact.h
-//  Gauss
-//
-//  Created by zero on 3/11/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
+#import "appdel.h"
 
-#import <Foundation/Foundation.h>
-
-@interface ccontact : NSObject
+@interface ccontact:
 
 @end
