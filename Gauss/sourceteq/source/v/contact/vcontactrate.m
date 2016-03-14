e8e03abef98a268074d8368a1afb1ba32078c609
vauxhall <zero.griffin@gmail.com>
2016-03-14 17:08:10 -0600

    model col

--- Gauss/sourceteq/source/v/contact/vcontactrate.m
+++ Gauss/sourceteq/source/v/contact/vcontactrate.m
@@ -1,5 +1,8 @@
 #import "vcontactrate.h"
 
+static NSString* const ratecell = @"cellid";
+static NSUInteger const colheight = 100;
+
 @implementation vcontactrate
 
 -(instancetype)initWithFrame:(CGRect)frame
@@ -16,13 +19,34 @@
     [label setTextColor:colorthird];
     [label setBackgroundColor:[UIColor clearColor]];
     
-    [self addSubview:label];
+    UICollectionViewFlowLayout *flow = [[UICollectionViewFlowLayout alloc] init];
+    [flow setHeaderReferenceSize:CGSizeZero];
+    [flow setFooterReferenceSize:CGSizeZero];
+    [flow setMinimumInteritemSpacing:0];
+    [flow setMinimumLineSpacing:0];
+    [flow setScrollDirection:UICollectionViewScrollDirectionHorizontal];
     
-    NSDictionary *views = @{};
-    NSDictionary *metrics = @{};
+    UICollectionView *collection = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:flow];
+    [collection setClipsToBounds:YES];
+    [collection setBackgroundColor:[UIColor clearColor]];
+    [collection setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [collection setScrollEnabled:NO];
+    [collection setBounces:NO];
+    [collection setDataSource:self];
+    [collection setDelegate:self];
+    [collection registerClass:[vcontactratecell class] forCellWithReuseIdentifier:ratecell];
+    self.collection = collection;
+    
+    [self addSubview:label];
+    [self addSubview:collection];
+    
+    NSDictionary *views = @{@"label":label, @"col":collection};
+    NSDictionary *metrics = @{@"colheight":@(colheight)};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[label]-20-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-20-[label]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[col]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[col(colheight)]-0-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
