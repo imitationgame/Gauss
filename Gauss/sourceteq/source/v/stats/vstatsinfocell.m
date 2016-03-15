2a15fae92951cff9c93e1a0380b09694a279368b
vauxhall <zero.griffin@gmail.com>
2016-03-15 16:47:09 -0600

    bottom border in stats cell

--- Gauss/sourceteq/source/v/stats/vstatsinfocell.m
+++ Gauss/sourceteq/source/v/stats/vstatsinfocell.m
@@ -1,14 +1,29 @@
 #import "vstatsinfocell.h"
 
+static NSUInteger const bottomborderheight = 6;
+
 @implementation vstatsinfocell
 
 -(instancetype)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     [self setClipsToBounds:YES];
-//    [self setBackgroundColor:[UIColor clearColor]];
     [self setBackgroundColor:colormain];
     [self setUserInteractionEnabled:NO];
+ 
+    UIView *bottomborder = [[UIView alloc] init];
+    [bottomborder setBackgroundColor:colorsecond];
+    [bottomborder setUserInteractionEnabled:NO];
+    [bottomborder setClipsToBounds:YES];
+    [bottomborder setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    [self addSubview:bottomborder];
+    
+    NSDictionary *views = @{@"bottomborder":bottomborder};
+    NSDictionary *metrics = @{@"bottomborderheight":@(bottomborderheight)};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[bottomborder]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[bottomborder(bottomborderheight)]-0-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
