51d1f6202c8291846e31a69f59acff7990f90308
vauxhall <zero.griffin@gmail.com>
2016-03-15 14:40:21 -0600

    fields

--- Gauss/sourceteq/source/v/contact/vcontactwrite.m
+++ Gauss/sourceteq/source/v/contact/vcontactwrite.m
@@ -19,11 +19,33 @@
     [labelgeneraltitle setTextColor:[UIColor colorWithWhite:0 alpha:0.4]];
     [labelgeneraltitle setText:NSLocalizedString(@"contact_write_generaltitle", nil)];
     
+    UILabel *labelnametitle = [[UILabel alloc] init];
+    [labelnametitle setBackgroundColor:[UIColor clearColor]];
+    [labelnametitle setUserInteractionEnabled:NO];
+    [labelnametitle setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [labelnametitle setFont:[UIFont fontWithName:fontregularname size:18]];
+    [labelnametitle setTextColor:[UIColor colorWithWhite:0 alpha:0.6]];
+    [labelnametitle setText:NSLocalizedString(@"contact_write_nametitle", nil)];
+    
+    UILabel *labelemailtitle = [[UILabel alloc] init];
+    [labelemailtitle setBackgroundColor:[UIColor clearColor]];
+    [labelemailtitle setUserInteractionEnabled:NO];
+    [labelemailtitle setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [labelemailtitle setFont:[UIFont fontWithName:fontregularname size:18]];
+    [labelemailtitle setTextColor:[UIColor colorWithWhite:0 alpha:0.6]];
+    [labelemailtitle setText:NSLocalizedString(@"contact_write_emailtitle", nil)];
+    
     UIView *fieldnamebase = [[UIView alloc] init];
     [fieldnamebase setBackgroundColor:[colorsecond colorWithAlphaComponent:0.4]];
     [fieldnamebase setClipsToBounds:YES];
     [fieldnamebase.layer setCornerRadius:4];
     [fieldnamebase setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    UIView *fieldemailbase = [[UIView alloc] init];
+    [fieldemailbase setBackgroundColor:[colorsecond colorWithAlphaComponent:0.4]];
+    [fieldemailbase setClipsToBounds:YES];
+    [fieldemailbase.layer setCornerRadius:4];
+    [fieldemailbase setTranslatesAutoresizingMaskIntoConstraints:NO];
     
     UITextField *fieldname = [[UITextField alloc] init];
     [fieldname setAutocapitalizationType:UITextAutocapitalizationTypeWords];
@@ -42,10 +64,30 @@
     [fieldname setTranslatesAutoresizingMaskIntoConstraints:NO];
     self.fieldname = fieldname;
     
-    [fieldnamebase addSubview:fieldname];
+    UITextField *fieldemail = [[UITextField alloc] init];
+    [fieldemail setAutocapitalizationType:UITextAutocapitalizationTypeWords];
+    [fieldemail setAutocorrectionType:UITextAutocorrectionTypeNo];
+    [fieldemail setBackgroundColor:[UIColor clearColor]];
+    [fieldemail setBorderStyle:UITextBorderStyleNone];
+    [fieldemail setClearButtonMode:UITextFieldViewModeNever];
+    [fieldemail setDelegate:self];
+    [fieldemail setFont:font];
+    [fieldemail setKeyboardAppearance:UIKeyboardAppearanceLight];
+    [fieldemail setKeyboardType:UIKeyboardTypeAlphabet];
+    [fieldemail setReturnKeyType:UIReturnKeyNext];
+    [fieldemail setSpellCheckingType:UITextSpellCheckingTypeNo];
+    [fieldemail setTextColor:textcolor];
+    [fieldemail setTintColor:textcolor];
+    [fieldemail setTranslatesAutoresizingMaskIntoConstraints:NO];
+    self.fieldemail = fieldemail;
     
+    [fieldnamebase addSubview:fieldname];
+    [fieldemailbase addSubview:fieldemail];
     [self addSubview:labelgeneraltitle];
+    [self addSubview:labelnametitle];
+    [self addSubview:labelemailtitle];
     [self addSubview:fieldnamebase];
+    [self addSubview:fieldemailbase];
     
     /*
      
@@ -55,14 +97,19 @@
      
      */
     
-    NSDictionary *views = @{@"labelgeneral":labelgeneraltitle, @"fieldname":fieldname, @"namebase":fieldnamebase};
+    NSDictionary *views = @{@"labelgeneral":labelgeneraltitle, @"fieldname":fieldname, @"namebase":fieldnamebase, @"labelname":labelnametitle, @"labelemail":labelemailtitle, @"emailbase":fieldemailbase};
     NSDictionary *metrics = @{};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[labelgeneral]-20-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[labelname]-20-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[labelemail]-20-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[namebase]-20-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[emailbase]-20-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-8-[fieldname]-5-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[fieldname]-0-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-30-[labelgeneral]-10-[namebase(50)]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-8-[fieldemail]-5-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[fieldemail]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-30-[labelgeneral]-20-[labelname]-10-[namebase(50)]-50-[labelemail]-10-[emailbase(50)]" options:0 metrics:metrics views:views]];
     
     return self;
 }
