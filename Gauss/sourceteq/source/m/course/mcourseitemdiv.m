9da1cf54743d512e0c303e828354822a0903dbe4
vauxhall <zero.griffin@gmail.com>
2016-03-15 19:48:37 -0600

    models

--- Gauss/sourceteq/source/m/course/mcourseitemdiv.m
+++ Gauss/sourceteq/source/m/course/mcourseitemdiv.m
@@ -1,13 +1,20 @@
-//
-//  mcourseitemdiv.m
-//  Gauss
-//
-//  Created by zero on 3/15/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
-
 #import "mcourseitemdiv.h"
 
 @implementation mcourseitemdiv
+
+-(instancetype)init
+{
+    self = [super init];
+    self.name = NSLocalizedString(@"course_div_name", nil);
+    
+    [self add:[[mcoursediv1 alloc] init]];
+    [self add:[[mcoursediv2 alloc] init]];
+    [self add:[[mcoursediv3 alloc] init]];
+    [self add:[[mcoursediv4 alloc] init]];
+    [self add:[[mcoursediv5 alloc] init]];
+    [self add:[[mcoursediv6 alloc] init]];
+    
+    return self;
+}
 
 @end
--- Gauss/sourceteq/source/m/course/mcourseitemmult.m
+++ Gauss/sourceteq/source/m/course/mcourseitemmult.m
@@ -1,13 +1,20 @@
-//
-//  mcourseitemmult.m
-//  Gauss
-//
-//  Created by zero on 3/15/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
-
 #import "mcourseitemmult.h"
 
 @implementation mcourseitemmult
+
+-(instancetype)init
+{
+    self = [super init];
+    self.name = NSLocalizedString(@"course_mult_name", nil);
+    
+    [self add:[[mcoursemult1 alloc] init]];
+    [self add:[[mcoursemult2 alloc] init]];
+    [self add:[[mcoursemult3 alloc] init]];
+    [self add:[[mcoursemult4 alloc] init]];
+    [self add:[[mcoursemult5 alloc] init]];
+    [self add:[[mcoursemult6 alloc] init]];
+    
+    return self;
+}
 
 @end
