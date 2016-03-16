305fb66521c083dbd142d8a9404d2a958ef35136
vauxhall <zero.griffin@gmail.com>
2016-03-15 19:05:38 -0600

    course subs

--- Gauss/sourceteq/source/s/coursesubs/scoursesubs1.m
+++ Gauss/sourceteq/source/s/coursesubs/scoursesubs1.m
@@ -1,13 +1,16 @@
-//
-//  scoursesubs1.m
-//  Gauss
-//
-//  Created by zero on 3/15/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
-
 #import "scoursesubs1.h"
 
 @implementation scoursesubs1
+
+-(instancetype)init
+{
+    self = [super init];
+    
+    self.operations = @[
+                        [[moperationadd alloc] init]
+                        ];
+    
+    return self;
+}
 
 @end
