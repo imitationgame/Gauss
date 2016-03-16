da84570759e362969285808f14cf7a078310e5f2
vauxhall <zero.griffin@gmail.com>
2016-03-15 18:59:35 -0600

    model subs

--- Gauss/sourceteq/source/m/course/mcourseitemsubs.m
+++ Gauss/sourceteq/source/m/course/mcourseitemsubs.m
@@ -9,6 +9,10 @@
     
     [self add:[[mcoursesubs1 alloc] init]];
     [self add:[[mcoursesubs2 alloc] init]];
+    [self add:[[mcoursesubs3 alloc] init]];
+    [self add:[[mcoursesubs4 alloc] init]];
+    [self add:[[mcoursesubs5 alloc] init]];
+    [self add:[[mcoursesubs6 alloc] init]];
     
     return self;
 }
